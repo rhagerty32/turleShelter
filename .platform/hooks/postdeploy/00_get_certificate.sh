e272d0f4b114f604f8b7c5d8c5b4c1361c3680d0
rhagerty32 <ry2402@gmail.com>
2024-12-02 08:53:06 -0700

    update cert

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-#TODO: Replace the domain name with your own
-sudo certbot -n -d bakery.is404.net --nginx --agree-tos --email ryguy32@byu.edu
+sudo certbot -n -d turtleProject.is404.net --nginx --agree-tos --email ryguy32@byu.edu
