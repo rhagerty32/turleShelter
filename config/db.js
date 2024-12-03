08ec852c06144526f591baaf6d59ecde45a5eef3
benhagg <21bhaggard@gmail.com>
2024-12-03 13:59:44 -0700

    resolve merge conflicts in routes

--- config/db.js
+++ config/db.js
@@ -1,15 +1,22 @@
 // configuration and connection to the DB
 //TODO Change connection details to match new database
 const knex = require("knex")({
-    client: "pg",
-    connection: {
-        host: process.env.RDS_HOSTNAME || "localhost",
-        user: process.env.RDS_USERNAME || "postgres",
-        password: process.env.RDS_PASSWORD || "admin",
-        database: process.env.RDS_DB_NAME || "turtleShelter",
-        port: process.env.RDS_PORT || 5433,
-        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
-    }
+  client: "pg",
+  connection: {
+    // access the environment propreties as set up in Elastic Beanstalk
+    host: process.env.RDS_HOSTNAME || "localhost",
+    user: process.env.RDS_USERNAME || "postgres",
+    password: process.env.RDS_PASSWORD || "admin",
+    database: process.env.RDS_DB_NAME || "turtleShelter",
+    port: process.env.RDS_PORT || 5432,
+    // enable SSL to connect to the AWS database
+    ssl: process.env.DB_SSL
+      ? {
+          ca: fs.readFileSync("certs/us-east-1-bundle.pem"),
+          rejectUnauthorized: true,
+        }
+      : false,
+  },
 });
 
 module.exports = knex;
