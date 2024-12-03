3cf24a80d9510ba23893dff8b1b8f5733673d3ab
Ryan Hagerty <62524600+rhagerty32@users.noreply.github.com>
2024-12-02 19:49:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -39,19 +39,6 @@ app.use((req, res, next) => {
   next();
 });
 
-//TODO Change connection details to match new database
-const knex = require("knex")({
-    client: "pg",
-    connection: {
-        host: process.env.RDS_HOSTNAME || "localhost",
-        user: process.env.RDS_USERNAME || "postgres",
-        password: process.env.RDS_PASSWORD || "admin",
-        database: process.env.RDS_DB_NAME || "turtleShelter",
-        port: process.env.RDS_PORT || 5433,
-        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
-    }
-})
-
 app.get("/login", (req, res) => {
   res.render("pages/login", {
     originalUrl: req.session.originalUrl || "/",
